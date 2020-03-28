3b44f4cf744a7dca5d6c935b2f8a3cff360f7afd
Alejandro Ortega Rosas <19AlexOrtegaRosas97@users.noreply.github.com>
2020-03-28 01:44:26 -0600

    27-03-2020
    imagenes Responsive

--- src/app/material.ts
+++ src/app/material.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule}  from '@angular/material/sidenav';
+
 
 @NgModule({
     imports: [
@@ -14,6 +16,7 @@ import { MatSidenavModule}  from '@angular/material/sidenav';
         MatSidenavModule,
         MatButtonModule,
         MatListModule,
+        MatCardModule,
     ],
     exports: [
         MatToolbarModule,
@@ -21,6 +24,7 @@ import { MatSidenavModule}  from '@angular/material/sidenav';
         MatSidenavModule,
         MatButtonModule,
         MatListModule,
+        MatCardModule,
     ],
 })
 export class MaterialModule { }
